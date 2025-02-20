52e49cebcb5da57f1ffac3c195edd3f127a68314
Minwhan Shine <diabugatti@naver.com>
2025-02-20 14:18:19 -0500

    Corrected some comments

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ def home():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # SQLite 파일 위치
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 불필요한 설정 끄기
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # SQLite File Location
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Turning off unnecessary settings 
 db = SQLAlchemy(app)
 
 
@@ -24,7 +24,7 @@ conn = sqlite3.connect('sensor_data.db')
 # Create cursor
 cursor = conn.cursor()
 
-# Create Table 테이블 생성 SQL 실행
+# Create Table, implementing SQL
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS SensorData (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
