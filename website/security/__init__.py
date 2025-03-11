b8fb27c45cd156e5b06cebac3f76d8993c883b5f
Aghashh Perumalz <aghashhp@gmail.com>
2025-03-11 10:36:58 -0400

    changed website design

--- website/security/__init__.py
+++ website/security/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'security.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'security.sqlite')
     )
 
     @app.route('/')
